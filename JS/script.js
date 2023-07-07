c28bc145b472317d67c5d7a46d42c9e3b0277ab9
YoSoyIo <yosoyio057@gmail.com>
2023-07-07 12:03:40 -0600

    Actualización
    - Nuevo diseño
    - Nuevos iconos

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+function agregarProyectos(cantidad) {
+      var projectTile = document.querySelector('.project-tile');
+
+      for (var i = 1; i <= cantidad; i++) {
+        var proyectoHTML = `
+          <a href="" target="_blank">
+            <div class="pro-box">
+              <img src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute.jpg" alt="Imagen random" />
+              <p class="project-title">Proyecto ${i}</p>
+            </div>
+          </a>
+        `;
+
+        projectTile.innerHTML += proyectoHTML;
+      }
+
+      proyectoHTML = `
+        <a href="" target="_blank">
+          <div class="pro-box">
+            <img src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute.jpg" alt="Imagen random" />
+            <p class="project-title">Más proyectos...</p>
+          </div>
+        </a>
+      `;
+
+      projectTile.innerHTML += proyectoHTML;
+    }
+
+    // Ejemplo: agregar 3 proyectos
+    agregarProyectos(5);
