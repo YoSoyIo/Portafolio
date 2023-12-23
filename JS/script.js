5a6b0f5709e22efff359e2fdf095e42ffacd9174
YoSoyIo <yosoyio057@gmail.com>
2023-12-23 09:53:15 -0600

    Mejoras generales

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,36 @@
-function agregarProyectos(cantidad) {
-      var projectTile = document.querySelector('.project-tile');
+function agregarProyectos() {
+  var projectTile = document.querySelector('.project-tile');
+  var proyectos = JSON.parse(
+    fetch('JSON/project.json').then(response => response.text())
+  );
+  var numProyectos = proyectos.length;
 
-      for (var i = 1; i <= cantidad; i++) {
-        var proyectoHTML = `
-          <a href="" target="_blank">
-            <div class="pro-box">
-              <img src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute.jpg" alt="Imagen random" />
-              <p class="project-title">Proyecto ${i}</p>
-            </div>
-          </a>
-        `;
+  for (var i = 0; i < numProyectos; i++) {
+    var proyectoHTML = `
+      <a href="${proyectos[i].href}" target="_blank">
+        <div class="pro-box">
+          <img src="${proyectos[i].image}" alt="${proyectos[i].alt}" />
+          <p class="project-title">${proyectos[i].name}</p>
+        </div>
+      </a>
+    `;
 
-        projectTile.innerHTML += proyectoHTML;
-      }
+    projectTile.innerHTML += proyectoHTML;
+  }
 
-      proyectoHTML = `
-        <a href="" target="_blank">
-          <div class="pro-box">
-            <img src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute.jpg" alt="Imagen random" />
-            <p class="project-title">Más proyectos...</p>
-          </div>
-        </a>
-      `;
+  proyectoHTML = `
+    <a href="" target="_blank">
+      <div class="pro-box">
+        <img src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute.jpg" alt="Imagen random" />
+        <p class="project-title">Más proyectos...</p>
+      </div>
+    </a>
+  `;
 
-      projectTile.innerHTML += proyectoHTML;
-    }
+  projectTile.innerHTML += proyectoHTML;
+}
+
+
 
     // Ejemplo: agregar 3 proyectos
-    agregarProyectos(5);
+    agregarProyectos();
